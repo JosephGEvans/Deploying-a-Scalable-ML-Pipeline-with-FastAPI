5ba95ab802937822f7d4283158ed2e365d76f349
JosephGEvans <jsphevans@hotmail.com>
2025-10-17 10:11:07 -0400

    Created 3 unit tests for the ml pipeline

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,45 @@
 import pytest
-# TODO: add necessary import
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+from train_model import cat_features, label, X_train, X_test, y_train, y_test, model
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+@pytest.fixture(scope="session")
+def raw_data():
     """
-    # add description for the second test
+    Create a Pandas DataFrame object with the expected raw data input.
     """
-    # Your code here
-    pass
+    df = pd.read_csv("./data/census.csv")
+    return df
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_raw_data_features(raw_data):
     """
-    # add description for the third test
+    Ensure the raw input data has the expected feature and label columns used in processing and training.
     """
-    # Your code here
-    pass
+    # Create a set of actual feature names
+    raw_data_features = set(raw_data.columns)
+    # Create a set of expected feature names
+    expected_features = set(cat_features)
+    # Add the label to the set of expected feature names
+    expected_features.add(label)
+
+    # Test that all the expected features exist in the raw data
+    assert expected_features.issubset(raw_data_features)
+
+
+def test_processed_data_consistency():
+    """
+    Verifies the training and test data sets have consistent feature width and data types.
+    """
+    assert X_train.shape[1] == X_test.shape[1]
+    assert X_train.dtype == X_test.dtype
+    assert y_train.dtype == y_test.dtype
+
+
+def test_model_algorithm():
+    """
+    The model algorithm is expected to be a Random Forest Classifier
+    """
+    assert isinstance(model, RandomForestClassifier)
